b2632dd97a3e4f98dff30cc7d951abba4522f6bf
David Porta <51144828+DPorta@users.noreply.github.com>
2023-03-30 20:32:14 -0500

    Download image, take photo and edit image buttons added

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ToastAndroid,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 import * as Sharing from "expo-sharing";
+import * as ImageManipulator from "expo-image-manipulator";
+import * as MediaLibrary from "expo-media-library";
+import { Camera } from "expo-camera";
 
 export default function App() {
   const [imageUri, setImageUri] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [cameraActive, setCameraActive] = useState(false);
 
   //Peticion de permisos
   useEffect(() => {
     (async () => {
       const { status } = await Permissions.askAsync(
         Permissions.CAMERA_ROLL,
-        Permissions.CAMERA
+        Permissions.CAMERA,
+        Permissions.MEDIA_LIBRARY
       );
       if (status !== "granted") {
         alert(
@@ -31,10 +44,38 @@ export default function App() {
       allowsEditing: true,
       quality: 1,
     });
-
-    if (!result.cancelled) {
-      setImageUri(result.uri);
+    if (!result.canceled) {
+      setImageUri(result.assets[0].uri);
     }
+  };
+  //Tomar foto con camara de usuario
+  const takePicture = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.takePictureAsync();
+      console.log(photo);
+      setImageUri(photo.uri);
+      setCameraActive(false);
+      // Aquí puedes hacer lo que quieras con la foto
+    }
+  };
+  //Editar la imagen
+  const editImage = async () => {
+    const imgUri = imageUri;
+    const editedImage = await ImageManipulator.manipulateAsync(imgUri, [], {
+      format: "png",
+    });
+    console.log(editedImage);
+    setImageUri(editedImage.uri);
+  };
+  //Descargar la imagen
+  const downloadImage = async () => {
+    const asset = await MediaLibrary.createAssetAsync(imageUri);
+    await MediaLibrary.createAlbumAsync("PhoRest", asset, false);
+    console.log(asset);
+    ToastAndroid.show(
+      "La imagen se ha descargado con éxito.",
+      ToastAndroid.SHORT
+    );
   };
   //Compartir la imagen
   const shareImage = async () => {
@@ -42,6 +83,7 @@ export default function App() {
       await Sharing.shareAsync(imageUri);
     } catch (error) {
       console.log(error);
+      alert(error);
     }
   };
   //Test
@@ -102,18 +144,53 @@ export default function App() {
         <TouchableOpacity onPress={pickImage} style={styles.button}>
           <Feather name="image" size={24} color="white" />
         </TouchableOpacity>
-        <TouchableOpacity onPress={test} style={styles.button}>
+        <TouchableOpacity
+          onPress={() => setCameraActive(true)}
+          style={styles.button}
+        >
           <Feather name="camera" size={24} color="white" />
         </TouchableOpacity>
         {imageUri && (
+          <TouchableOpacity onPress={editImage} style={styles.button}>
+            <Feather name="edit" size={24} color="white" />
+          </TouchableOpacity>
+        )}
+        {imageUri && (
+          <TouchableOpacity onPress={downloadImage} style={styles.button}>
+            <Feather name="download" size={24} color="white" />
+          </TouchableOpacity>
+        )}
+        {imageUri && (
           <TouchableOpacity onPress={shareImage} style={styles.button}>
-            <Feather name="share" size={24} color="white" />
+            <Feather name="share-2" size={24} color="white" />
           </TouchableOpacity>
         )}
       </View>
       <TouchableOpacity onPress={test} style={styles.Sbutton}>
         <Text style={styles.myText}>Restaurar</Text>
       </TouchableOpacity>
+
+      {cameraActive && (
+        <Camera
+          style={styles.camera}
+          type={Camera.Constants.Type.back}
+          ref={(ref) => setCameraRef(ref)}
+        >
+          <View style={styles.buttonCamContainer}>
+            <TouchableOpacity
+              style={styles.buttonCam}
+              onPress={() => takePicture()}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              style={styles.buttonCancelCam}
+              onPress={() => setCameraActive(false)}
+            >
+              <Feather name="x-circle" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )}
+
       <StatusBar style="inverted" />
     </View>
   );
@@ -149,7 +226,7 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
     width: "60%",
     marginTop: 15,
   },
@@ -171,5 +248,39 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  camera: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  buttonCamContainer: {
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    paddingVertical: 20,
+  },
+  buttonCam: {
+    width: 70,
+    height: 70,
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    borderWidth: 5,
+    borderColor: "#000",
+  },
+  buttonCancelCam: {
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    right: 20,
+    width: 35,
+    height: 35,
+    backgroundColor: "black",
+    borderRadius: 50,
+    borderWidth: 5,
+    borderColor: "#000",
   },
 });
